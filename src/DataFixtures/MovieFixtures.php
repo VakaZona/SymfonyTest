be68ac9d3da8c449a94d498d3db609a8dc503447
zonov <zonovvaleraooo@gmail.com>
2022-10-26 13:43:55 +0700

    Add webapp packages

---
+++ src/DataFixtures/MovieFixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Movie;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class MovieFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $movie = new Movie();
+        $movie->setTitle('The Dark Knight');
+        $movie->setReleaseYear(2008);
+        $movie->setDescription('This is the description of the Dark Knight');
+        $movie->setImagePath('https://resizing.flixster.com/WAHXGKleT3QvhqHUlFGIRgcQAjU=/206x305/v2/https://flxt.tmsimg.com/assets/p173378_p_v8_au.jpg');
+        $movie->addActor($this->getReference('actor_1'));
+        $movie->addActor($this->getReference('actor_2'));
+        $manager->persist($movie);
+
+        $movie2 = new Movie();
+        $movie2->setTitle('Avengers: Endgame');
+        $movie2->setReleaseYear(2019);
+        $movie2->setDescription('This is the description of the Avengers: Endgame');
+        $movie2->setImagePath('https://play-lh.googleusercontent.com/XDg-bt655am_Q-7X-I0s64Kq8SJKfb7BBTHkUVbFR6-zDNv9J7rW61xZn0BB3SVCJ6gz ');
+        $movie2->addActor($this->getReference('actor_3'));
+        $movie2->addActor($this->getReference('actor_4'));
+        $manager->persist($movie2);
+
+        $manager->flush();
+
+
+    }
+}
