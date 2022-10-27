1b74759ade18b3d0016d980099f34d1af18bb07a
zonov <zonovvaleraooo@gmail.com>
2022-10-27 15:15:36 +0700

    Add webapp packages

---
+++ src/Controller/Admin/MovieCrudController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Movie;
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use EasyCorp\Bundle\EasyAdminBundle\Config\KeyValueStore;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class MovieCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Movie::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setPaginatorPageSize(10)
+            ->setPaginatorRangeSize(4)
+            ->setEntityPermission('ROLE_ADMIN')
+            ;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IntegerField::new('id','ID')->onlyOnIndex(),
+            TextField::new('Title'),
+            IntegerField::new('release_year'),
+            TextareaField::new('description'),
+            ImageField::new('image_path')->setUploadDir('public/uploads/')->setUploadedFileNamePattern('/uploads/[contenthash].[extension]'),
+        ];
+    }
+}
